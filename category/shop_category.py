1e9c32835f7079f17b3012a560c706d168f32d73
kevin-zx <519754821@qq.com>
2017-01-10 09:39:18 +0800

    add category

--- category/shop_category.py
+++ category/shop_category.py
@@ -3,8 +3,8 @@ from category import Category
 
 
 class ShopCategory(Category):
-    def __init__(self, category_id, parent_category_id, root_id, category_name, add_cart_item_cnt, cate_item_cnt, cate_uv,
-                 order_rate):
+    def __init__(self, category_id, parent_category_id, root_id, category_name, add_cart_item_cnt, cate_item_cnt,
+                 cate_uv, order_rate, is_children):
         """
         店铺本身经营的一些类目
         :param category_id:
@@ -15,12 +15,13 @@ class ShopCategory(Category):
         :param cate_item_cnt:
         :param cate_uv:
         :param order_rate:
+        :param is_children
         """
         super(ShopCategory, self).__init__(category_id, parent_category_id, root_id, category_name)
         self.add_cart_item_cnt = add_cart_item_cnt
         self.cate_item_cnt = cate_item_cnt
         self.cate_uv = cate_uv
         self.order_rate = order_rate
-
-    # def __str__(self):
-    #     pass
+        self.is_children = is_children
+        # def __str__(self):
+        #     pass
